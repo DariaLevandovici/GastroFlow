e09f146df86c8b73de511cec4c3bd0eff934c988
Levandovici Daria <daria.levandovici@isa.utm.md>
2026-05-03 17:42:33 +0300

    am adaugat api pentru imagini

--- my-react-app/src/config/api.ts
+++ my-react-app/src/config/api.ts
@@ -4,4 +4,5 @@ export const API_BASE_URL = 'http://localhost:5224';
 export const API_ENDPOINTS = {
   products: `${API_BASE_URL}/api/Products`,
   ingredients: "http://localhost:5224/api/Ingredients",
+  images: `${API_BASE_URL}/api/Images`,
 } as const;
